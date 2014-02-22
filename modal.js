990ae2b31a0b044dc890f7d9eb950e04970f7c16
Amio <amio.cn@gmail.com>
2014-02-22 17:46:48 +0800

    Adjust indent

--- modal.js
+++ modal.js
@@ -9,66 +9,66 @@
 angular.module('btford.modal', []).
 factory('btfModal', ['$compile, $rootScope, $controller, $q, $http, $templateCache',
   function ($compile, $rootScope, $controller, $q, $http, $templateCache) {
-  return function modalFactory (config) {
+    return function modalFactory (config) {
 
-    if ((+!!config.template) + (+!!config.templateUrl) !== 1) {
-      throw new Error('Expected modal to have exacly one of either `template` or `templateUrl`');
-    }
+      if ((+!!config.template) + (+!!config.templateUrl) !== 1) {
+        throw new Error('Expected modal to have exacly one of either `template` or `templateUrl`');
+      }
 
-    var template      = config.template,
-        controller    = config.controller || angular.noop,
-        controllerAs  = config.controllerAs,
-        container     = angular.element(config.container || document.body),
-        element       = null,
-        html;
+      var template      = config.template,
+          controller    = config.controller || angular.noop,
+          controllerAs  = config.controllerAs,
+          container     = angular.element(config.container || document.body),
+          element       = null,
+          html;
 
-    if (config.template) {
-      var deferred = $q.defer();
-      deferred.resolve(config.template);
-      html = deferred.promise;
-    } else {
-      html = $http.get(config.templateUrl, {
-        cache: $templateCache
-      }).
-      then(function (response) {
-        return response.data;
-      });
-    }
+      if (config.template) {
+        var deferred = $q.defer();
+        deferred.resolve(config.template);
+        html = deferred.promise;
+      } else {
+        html = $http.get(config.templateUrl, {
+          cache: $templateCache
+        }).
+        then(function (response) {
+          return response.data;
+        });
+      }
 
-    function activate (locals) {
-      html.then(function (html) {
-        if (!element) {
-          attach(html, locals);
+      function activate (locals) {
+        html.then(function (html) {
+          if (!element) {
+            attach(html, locals);
+          }
+        });
+      }
+
+      function attach (html, locals) {
+        element = angular.element(html);
+        container.prepend(element);
+        var scope = $rootScope.$new();
+        if (locals) {
+          for (var prop in locals) {
+            scope[prop] = locals[prop];
+          }
         }
-      });
-    }
+        var ctrl = $controller(controller, { $scope: scope });
+        if (controllerAs) {
+          scope[controllerAs] = ctrl;
+        }
+        $compile(element)(scope);
+      }
 
-    function attach (html, locals) {
-      element = angular.element(html);
-      container.prepend(element);
-      var scope = $rootScope.$new();
-      if (locals) {
-        for (var prop in locals) {
-          scope[prop] = locals[prop];
+      function deactivate () {
+        if (element) {
+          element.remove();
+          element = null;
         }
       }
-      var ctrl = $controller(controller, { $scope: scope });
-      if (controllerAs) {
-        scope[controllerAs] = ctrl;
-      }
-      $compile(element)(scope);
-    }
 
-    function deactivate () {
-      if (element) {
-        element.remove();
-        element = null;
-      }
-    }
-
-    return {
-      activate: activate,
-      deactivate: deactivate
+      return {
+        activate: activate,
+        deactivate: deactivate
+      };
     };
-  };
 }]);
